212da5afb33acf63806f81bdb3675824dc6a42a0
cy9her <30275000+cy9her@users.noreply.github.com>
2020-04-11 08:06:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
- "here is some web content" 
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to you WebApp</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
+  </head>
+  <body>
+
+      <h1>Please Login or SignUp</h1>
+
+      <a href="login.php">Login</a> or
+      <a href="signup.php">SignUp</a>
+    <?php endif; ?>
+  </body>
+</html>
